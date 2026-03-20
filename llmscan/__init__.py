d239328e1582b010c010effd98bd2eed4e71fb3c
julienbrasseur <69792046+julienbrasseur@users.noreply.github.com>
2026-03-20 11:18:32 +0100

    Update __init__.py

--- llmscan/__init__.py
+++ llmscan/__init__.py
@@ -3,6 +3,12 @@ from .attention_extractor import AttentionExtractor, AVAILABLE_STATS
 from .layer_selector import layer_selection_pipeline
 from .xgboost_probe import XGBoostProbe
 from .tcn import TCNProbe
+from .attention_probe import (
+    AttentionProbe,
+    train_attention_probe,
+    evaluate_attention_probe,
+    inspect_layer_attention
+)
 
 __all__ = [
     "ActivationExtractor",
@@ -10,5 +16,9 @@ __all__ = [
     "AVAILABLE_STATS",
     "layer_selection_pipeline",
     "XGBoostProbe",
-    "TCNProbe"
+    "TCNProbe",
+    "AttentionProbe",
+    "train_attention_probe",
+    "evaluate_attention_probe",
+    "inspect_layer_attention"
 ]
